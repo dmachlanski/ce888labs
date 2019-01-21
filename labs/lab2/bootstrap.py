1993a9cc3baa674553e1f6b4af4c4ce7d78a33b3
Machlanski <dm18389@essex.ac.uk>
2019-01-21 14:55:35 +0000

    Exercise 2 of Bootstrap

--- labs/lab2/bootstrap.py
+++ labs/lab2/bootstrap.py
@@ -44,6 +44,44 @@ if __name__ == "__main__":
 	sns_plot.savefig("bootstrap_confidence.png", bbox_inches='tight')
 	sns_plot.savefig("bootstrap_confidence.pdf", bbox_inches='tight')
 
-
 	#print ("Mean: %f")%(np.mean(data))
 	#print ("Var: %f")%(np.var(data))
+
+	#Exercise 2 (vehicles)
+
+	df_veh = pd.read_csv('./vehicles.csv')
+
+	df_old_veh = df_veh['Current fleet'].values
+	df_new_veh = df_veh['New Fleet'].values
+
+	boots = []
+	for i in range(100, 100000, 1000):
+		boot = bootstrap(df_old_veh, df_old_veh.shape[0], i, 95.0)
+		boots.append([i, boot[0], "mean"])
+		boots.append([i, boot[1], "lower"])
+		boots.append([i, boot[2], "upper"])
+
+	df_boot = pd.DataFrame(boots, columns=['Boostrap Iterations', 'Mean', "Value"])
+	sns_plot = sns.lmplot(df_boot.columns[0], df_boot.columns[1], data=df_boot, fit_reg=False, hue="Value")
+
+	sns_plot.axes[0, 0].set_ylim(0, )
+	sns_plot.axes[0, 0].set_xlim(0, 100000)
+
+	sns_plot.savefig("bootstrap_confidence_old_fleet.png", bbox_inches='tight')
+	sns_plot.savefig("bootstrap_confidence_old_fleet.pdf", bbox_inches='tight')
+
+	boots = []
+	for i in range(100, 100000, 1000):
+		boot = bootstrap(df_new_veh, df_new_veh.shape[0], i, 95.0)
+		boots.append([i, boot[0], "mean"])
+		boots.append([i, boot[1], "lower"])
+		boots.append([i, boot[2], "upper"])
+
+	df_boot = pd.DataFrame(boots, columns=['Boostrap Iterations', 'Mean', "Value"])
+	sns_plot = sns.lmplot(df_boot.columns[0], df_boot.columns[1], data=df_boot, fit_reg=False, hue="Value")
+
+	sns_plot.axes[0, 0].set_ylim(0, )
+	sns_plot.axes[0, 0].set_xlim(0, 100000)
+
+	sns_plot.savefig("bootstrap_confidence_new_fleet.png", bbox_inches='tight')
+	sns_plot.savefig("bootstrap_confidence_new_fleet.pdf", bbox_inches='tight')

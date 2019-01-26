5eded49c9dcf8fe17b7aea4338fa73898b0c3a38
dmachlanski <damian.machlanski@gmail.com>
2019-01-26 17:25:49 +0000

    Lab 2: Bootstrap part 2

--- labs/lab2/bootstrap.py
+++ labs/lab2/bootstrap.py
@@ -22,12 +22,8 @@ def bootstrap(sample, sample_size, iterations, ci):
 
 	return data_mean, lower, upper
 
-
-if __name__ == "__main__":
-
-	df = pd.read_csv('./salaries.csv')
-
-	data = df.values.T[1]
+def process_data(data, output):
+	
 	boots = []
 	for i in range(100, 100000, 1000):
 		boot = bootstrap(data, data.shape[0], i, 95.0)
@@ -41,47 +37,22 @@ if __name__ == "__main__":
 	sns_plot.axes[0, 0].set_ylim(0,)
 	sns_plot.axes[0, 0].set_xlim(0, 100000)
 
-	sns_plot.savefig("bootstrap_confidence.png", bbox_inches='tight')
-	sns_plot.savefig("bootstrap_confidence.pdf", bbox_inches='tight')
+	sns_plot.savefig(f"bootstrap_confidence_{output}.png", bbox_inches='tight')
+	sns_plot.savefig(f"bootstrap_confidence_{output}.pdf", bbox_inches='tight')
 
-	#print ("Mean: %f")%(np.mean(data))
-	#print ("Var: %f")%(np.var(data))
+if __name__ == "__main__":
+
+	df = pd.read_csv('./salaries.csv')
+	data = df.values.T[1]
+	
+	process_data(data, "salaries")
 
 	#Exercise 2 (vehicles)
 
 	df_veh = pd.read_csv('./vehicles.csv')
 
-	df_old_veh = df_veh['Current fleet'].values
-	df_new_veh = df_veh['New Fleet'].dropna().values
+	old_vehs = df_veh['Current fleet'].values
+	new_vehs = df_veh['New Fleet'].dropna().values
 
-	boots = []
-	for i in range(100, 100000, 1000):
-		boot = bootstrap(df_old_veh, df_old_veh.shape[0], i, 95.0)
-		boots.append([i, boot[0], "mean"])
-		boots.append([i, boot[1], "lower"])
-		boots.append([i, boot[2], "upper"])
-
-	df_boot = pd.DataFrame(boots, columns=['Boostrap Iterations', 'Mean', "Value"])
-	sns_plot = sns.lmplot(df_boot.columns[0], df_boot.columns[1], data=df_boot, fit_reg=False, hue="Value")
-
-	sns_plot.axes[0, 0].set_ylim(0, )
-	sns_plot.axes[0, 0].set_xlim(0, 100000)
-
-	sns_plot.savefig("bootstrap_confidence_old_fleet.png", bbox_inches='tight')
-	sns_plot.savefig("bootstrap_confidence_old_fleet.pdf", bbox_inches='tight')
-
-	boots = []
-	for i in range(100, 100000, 1000):
-		boot = bootstrap(df_new_veh, df_new_veh.shape[0], i, 95.0)
-		boots.append([i, boot[0], "mean"])
-		boots.append([i, boot[1], "lower"])
-		boots.append([i, boot[2], "upper"])
-
-	df_boot = pd.DataFrame(boots, columns=['Boostrap Iterations', 'Mean', "Value"])
-	sns_plot = sns.lmplot(df_boot.columns[0], df_boot.columns[1], data=df_boot, fit_reg=False, hue="Value")
-
-	sns_plot.axes[0, 0].set_ylim(0, )
-	sns_plot.axes[0, 0].set_xlim(0, 100000)
-
-	sns_plot.savefig("bootstrap_confidence_new_fleet.png", bbox_inches='tight')
-	sns_plot.savefig("bootstrap_confidence_new_fleet.pdf", bbox_inches='tight')
+	process_data(old_vehs, "old_fleet")
+	process_data(new_vehs, "new_fleet")

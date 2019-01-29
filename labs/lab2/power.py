fab88c29a6e55fed267c1ad283af55219bc4ac53
dmachlanski <damian.machlanski@gmail.com>
2019-01-29 23:09:42 +0000

    Lab2: implemented power analysis

--- labs/lab2/power.py
+++ labs/lab2/power.py
@@ -1,10 +1,37 @@
 import numpy as np
 from random import choices
 
+def permutation_test(data1, data2, reps):
+
+	tobs = np.mean(data2) - np.mean(data1)
+	data_all = np.hstack((data1, data2))
+	size = len(data1)
+	
+	perm = []
+	for _ in range(reps):
+		perm_data = np.random.permutation(data_all)
+		pdata1 = perm_data[:size]
+		pdata2 = perm_data[-size:]
+		tperm = np.mean(pdata2) - np.mean(pdata1)
+		perm.append(tperm > tobs)
+	
+	return np.mean(perm)
+
 def power(sample1, sample2, reps, size, alpha):
 
-    for i in range(reps):
-        gen1 = choices(sample1, k=size)
-        gen2 = choices(sample2, k=size)
+	pvalues = []
+	for _ in range(reps):
+		data1 = choices(sample1, k=size)
+		data2 = choices(sample2, k=size)
+		pval = permutation_test(data1, data2, 19000)
+		pvalues.append(pval)
+	
+	return np.mean(np.array(pvalues) < (1.0-alpha))
 
-        
+if __name__ == "__main__":
+
+	old = np.array([0,0,0,0,0,0,1,0,0,1,0])
+	new = np.array([1,0,0,1,1,1,0,0,0,1,0])
+
+	p = power(old, new, 10, 11, 0.05)
+	print(f"Power = {p}")

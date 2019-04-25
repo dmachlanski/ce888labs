4b1fa8a067641b93a35ab861f0d773225db03902
dmachlanski <damian.machlanski@gmail.com>
2019-04-25 02:05:42 +0100

    Project: Remove redundant code.

--- project/UCT_DT.py
+++ project/UCT_DT.py
@@ -20,87 +20,6 @@ import random
 import pandas as pd
 from sklearn import tree
 
-class GameState:
-    """ A state of the game, i.e. the game board. These are the only functions which are
-        absolutely necessary to implement UCT in any 2-player complete information deterministic 
-        zero-sum game, although they can be enhanced and made quicker, for example by using a 
-        GetRandomMove() function to generate a random move during rollout.
-        By convention the players are numbered 1 and 2.
-    """
-    def __init__(self):
-            self.playerJustMoved = 2 # At the root pretend the player just moved is player 2 - player 1 has the first move
-        
-    def Clone(self):
-        """ Create a deep clone of this game state.
-        """
-        st = GameState()
-        st.playerJustMoved = self.playerJustMoved
-        return st
-
-    def DoMove(self, move):
-        """ Update a state by carrying out the given move.
-            Must update playerJustMoved.
-        """
-        self.playerJustMoved = 3 - self.playerJustMoved
-        
-    def GetMoves(self):
-        """ Get all possible moves from this state.
-        """
-    
-    def GetResult(self, playerjm):
-        """ Get the game result from the viewpoint of playerjm. 
-        """
-
-    def __repr__(self):
-        """ Don't need this - but good style.
-        """
-        pass
-
-
-class NimState:
-    """ A state of the game Nim. In Nim, players alternately take 1,2 or 3 chips with the 
-        winner being the player to take the last chip. 
-        In Nim any initial state of the form 4n+k for k = 1,2,3 is a win for player 1
-        (by choosing k) chips.
-        Any initial state of the form 4n is a win for player 2.
-    """
-    def __init__(self, ch):
-        self.playerJustMoved = 2 # At the root pretend the player just moved is p2 - p1 has the first move
-        self.chips = ch
-        
-    def Clone(self):
-        """ Create a deep clone of this game state.
-        """
-        st = NimState(self.chips)
-        st.playerJustMoved = self.playerJustMoved
-        return st
-
-    def DoMove(self, move):
-        """ Update a state by carrying out the given move.
-            Must update playerJustMoved.
-        """
-        assert move >= 1 and move <= 3 and move == int(move)
-        self.chips -= move
-        self.playerJustMoved = 3 - self.playerJustMoved
-        
-    def GetMoves(self):
-        """ Get all possible moves from this state.
-        """
-        return range(1,min([4, self.chips + 1]))
-    
-    def GetResult(self, playerjm):
-        """ Get the game result from the viewpoint of playerjm. 
-        """
-        assert self.chips == 0
-        if self.playerJustMoved == playerjm:
-            return 1.0 # playerjm took the last chip and has won
-        else:
-            return 0.0 # playerjm's opponent took the last chip and has won
-
-    def __repr__(self):
-        s = "Chips:" + str(self.chips) + " JustPlayed:" + str(self.playerJustMoved)
-        return s
-
 class OXOState:
     """ A state of the game, i.e. the game board.
         Squares in the board are in this arrangement
@@ -162,120 +81,6 @@ class OXOState:
         for i in range(9): 
             s += ".XO"[self.board[i]]
             if i % 3 == 2: s += "\n"
-        return s
-
-class OthelloState:
-    """ A state of the game of Othello, i.e. the game board.
-        The board is a 2D array where 0 = empty (.), 1 = player 1 (X), 2 = player 2 (O).
-        In Othello players alternately place pieces on a square board - each piece played
-        has to sandwich opponent pieces between the piece played and pieces already on the 
-        board. Sandwiched pieces are flipped.
-        This implementation modifies the rules to allow variable sized square boards and
-        terminates the game as soon as the player about to move cannot make a move (whereas
-        the standard game allows for a pass move). 
-    """
-    def __init__(self,sz = 8):
-        self.playerJustMoved = 2 # At the root pretend the player just moved is p2 - p1 has the first move
-        self.board = [] # 0 = empty, 1 = player 1, 2 = player 2
-        self.size = sz
-        assert sz == int(sz) and sz % 2 == 0 # size must be integral and even
-        for _ in range(sz):
-            self.board.append([0]*sz)
-        self.board[sz/2][sz/2] = self.board[sz/2-1][sz/2-1] = 1
-        self.board[sz/2][sz/2-1] = self.board[sz/2-1][sz/2] = 2
-
-    def Clone(self):
-        """ Create a deep clone of this game state.
-        """
-        st = OthelloState()
-        st.playerJustMoved = self.playerJustMoved
-        st.board = [self.board[i][:] for i in range(self.size)]
-        st.size = self.size
-        return st
-
-    def DoMove(self, move):
-        """ Update a state by carrying out the given move.
-            Must update playerToMove.
-        """
-        (x,y)=(move[0],move[1])
-        assert x == int(x) and y == int(y) and self.IsOnBoard(x,y) and self.board[x][y] == 0
-        m = self.GetAllSandwichedCounters(x,y)
-        self.playerJustMoved = 3 - self.playerJustMoved
-        self.board[x][y] = self.playerJustMoved
-        for (a,b) in m:
-            self.board[a][b] = self.playerJustMoved
-    
-    def GetMoves(self):
-        """ Get all possible moves from this state.
-        """
-        return [(x,y) for x in range(self.size) for y in range(self.size) if self.board[x][y] == 0 and self.ExistsSandwichedCounter(x,y)]
-
-    def AdjacentToEnemy(self,x,y):
-        """ Speeds up GetMoves by only considering squares which are adjacent to an enemy-occupied square.
-        """
-        for (dx,dy) in [(0,+1),(+1,+1),(+1,0),(+1,-1),(0,-1),(-1,-1),(-1,0),(-1,+1)]:
-            if self.IsOnBoard(x+dx,y+dy) and self.board[x+dx][y+dy] == self.playerJustMoved:
-                return True
-        return False
-    
-    def AdjacentEnemyDirections(self,x,y):
-        """ Speeds up GetMoves by only considering squares which are adjacent to an enemy-occupied square.
-        """
-        es = []
-        for (dx,dy) in [(0,+1),(+1,+1),(+1,0),(+1,-1),(0,-1),(-1,-1),(-1,0),(-1,+1)]:
-            if self.IsOnBoard(x+dx,y+dy) and self.board[x+dx][y+dy] == self.playerJustMoved:
-                es.append((dx,dy))
-        return es
-    
-    def ExistsSandwichedCounter(self,x,y):
-        """ Does there exist at least one counter which would be flipped if my counter was placed at (x,y)?
-        """
-        for (dx,dy) in self.AdjacentEnemyDirections(x,y):
-            if len(self.SandwichedCounters(x,y,dx,dy)) > 0:
-                return True
-        return False
-    
-    def GetAllSandwichedCounters(self, x, y):
-        """ Is (x,y) a possible move (i.e. opponent counters are sandwiched between (x,y) and my counter in some direction)?
-        """
-        sandwiched = []
-        for (dx,dy) in self.AdjacentEnemyDirections(x,y):
-            sandwiched.extend(self.SandwichedCounters(x,y,dx,dy))
-        return sandwiched
-
-    def SandwichedCounters(self, x, y, dx, dy):
-        """ Return the coordinates of all opponent counters sandwiched between (x,y) and my counter.
-        """
-        x += dx
-        y += dy
-        sandwiched = []
-        while self.IsOnBoard(x,y) and self.board[x][y] == self.playerJustMoved:
-            sandwiched.append((x,y))
-            x += dx
-            y += dy
-        if self.IsOnBoard(x,y) and self.board[x][y] == 3 - self.playerJustMoved:
-            return sandwiched
-        else:
-            return [] # nothing sandwiched
-
-    def IsOnBoard(self, x, y):
-        return x >= 0 and x < self.size and y >= 0 and y < self.size
-    
-    def GetResult(self, playerjm):
-        """ Get the game result from the viewpoint of playerjm. 
-        """
-        jmcount = len([(x,y) for x in range(self.size) for y in range(self.size) if self.board[x][y] == playerjm])
-        notjmcount = len([(x,y) for x in range(self.size) for y in range(self.size) if self.board[x][y] == 3 - playerjm])
-        if jmcount > notjmcount: return 1.0
-        elif notjmcount > jmcount: return 0.0
-        else: return 0.5 # draw
-
-    def __repr__(self):
-        s= ""
-        for y in range(self.size-1,-1,-1):
-            for x in range(self.size):
-                s += ".XO"[self.board[x][y]]
-            s += "\n"
         return s
 
 class Node:
@@ -403,6 +208,8 @@ def UCTPlayGame(model1, model2, early_stopping = False):
     return states, state.GetWinner()
 
 def UCTVsDTGame(dt, dt_index, itermax):
+    """ Play a game between UCT player and Decision Tree classifier.
+    """
     state = OXOState()
     winner = 0
 
@@ -471,7 +278,7 @@ if __name__ == "__main__":
                         l += 1
             e_vs_e.append([w, l, d])
             # MCTS vs DT
-            # - Play 100 games and save results as above
+            # - Play 100 games against each MCTS and save results as above
             for iters in uct_vs_dt:
                 w = l = d = 0
                 for k in range(100):
